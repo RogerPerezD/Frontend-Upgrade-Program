8e6510a6bb01e49b61596eccaa105b923dcfd596
Roger Perez <rogelio_perez_98@hotmail.com>
2021-12-28 16:51:00 -0600

    JavaScript Constructors

--- JavaScript/Objects and Prototypes In-depth/opp.js
+++ JavaScript/Objects and Prototypes In-depth/opp.js
@@ -7,17 +7,36 @@ emp1.lastName = 'Perez';
 emp1.gender = 'Male';
 emp1.designation = 'Developer';
 
-// Create object whit methods
+// Create object whit method
+// function createEmployeeObject( firstName, lastName, gender, designation){
+//     const newObject = {}; 
+//     newObject.firstName = firstName;
+//     newObject.lastName = lastName;
+//     newObject.gender = gender;
+//     newObject.designation = designation;
+//     return newObject;
+// }
+
+// Create object whit a constructor
+// In this case we don need create and return a object, Js make it automatically for us
+// and we can refer to the object whit the keywork this
 function createEmployeeObject( firstName, lastName, gender, designation){
-    const newObject = {}; 
-    newObject.firstName = firstName;
-    newObject.lastName = lastName;
-    newObject.gender = gender;
-    newObject.designation = designation;
-    return newObject;
+    // const this = {};  skip these steps
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.designation = designation;
+    // return this;
+
 }
 
-const emp2 = createEmployeeObject('Mike', 'Archundia', 'Male', 'Manager');
+// Create object whit method
+// const emp2 = createEmployeeObject('Mike', 'Archundia', 'Male', 'Manager');
+
+// Create object whit a constructor
+// The keyword new, tell JS that it invokes a constructor
+const emp2 = new createEmployeeObject('Mike', 'Archundia', 'Male', 'Manager');
+
 
 console.log(emp1);
 console.log(emp2)
