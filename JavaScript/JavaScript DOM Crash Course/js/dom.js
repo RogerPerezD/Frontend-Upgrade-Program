c1f364b48cc9a02d7cdec31d4aacc6c14b9c22e3
Roger Perez <rogelio_perez_98@hotmail.com>
2021-12-28 12:09:00 -0600

    Notes about traversing the dom: child, parent, siblings

--- JavaScript/JavaScript DOM Crash Course/js/dom.js
+++ JavaScript/JavaScript DOM Crash Course/js/dom.js
@@ -1,16 +1,102 @@
-const headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
+// Traversing the dom
 
-// Change style
-headerTitle.style.borderBottom = 'solid 3px #000';
+const itemList = document.querySelector('#items');
 
-// QuerySelector 
-const submit = document.querySelector('input[type="submit"]');
-submit.value = "Send";
+// get the parent Node
+const parentNodeList = itemList.parentNode;
+console.log(parentNodeList);
 
-const item = document.querySelector('.list-group-item');
-console.log(item);
-item.style.color = 'red';
+// change properties of parent node
+parentNodeList.style.backgroundColor = '#f4f4f4';
 
-const lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+// get parent Element
+// Es casi lo mismo que parent node, nos trae el nodo padre del elemento
+const parentElementList = itemList.parentElement;
+console.log(parentElementList);
+
+
+// Child nodes
+// Trae a los hijos del componente, pero si tiene enters, los
+// marca como si fueran espacios en blanco, marcando mas elementos
+// de los que hay en realidad
+console.log(itemList.childNodes);
+
+// children
+// Tambien nos trae a los nodos hijos del componente,
+// pero sin la problematica de childNodes,
+// por ende es mas recomendada usarla 
+console.log(itemList.children);
+
+// firstchild
+// Nos trae al primer elemento que encuentra,
+// Auunque sea un espacio en blanco, esto nos puede
+// traer problemas
+console.log(itemList.firstChild);
+
+// firstElementChild
+// Nos trae al primer nodo de HTMl que encuentra,
+// Es mejor que usar firstChild
+console.log(itemList.firstElementChild);
+
+// lastChild
+// Nos trae al ultimo elemento que encuentra,
+// Auunque sea un espacio en blanco, esto nos puede
+// traer problemas
+console.log(itemList.lastChild);
+
+// lastElementChild
+// Nos trae al ultimo nodo de HTMl que encuentra,
+// Es mejor que usar lastChild
+console.log(itemList.lastElementChild);
+
+// nextSibling
+// Nos trae al siguiente hermano del component, sufre del mismo
+// error de traer espacios en blancos en ciertas ocasiones
+console.log(itemList.nextSibling);
+
+/* nextElementSibling */
+// Nos trae al siguiente hermano del componente
+// Si no tiene un hermano siguiente, retorna null
+console.log(itemList.nextElementSibling);
+
+/* previousSibling */
+// Nos trae al anterior hermano del componente, sufre del mismo
+// error de traer espacios en blancos en ciertas ocasiones
+console.log(itemList.previousSibling);
+
+/* previousElementSibling */
+// Nos trae al anterior hermano del componente
+// Si no tiene un hermano anterior, retorna null
+console.log(itemList.previousElementSibling);
+
+/* Create Elemenet */
+// Create a div
+
+const newDiv = document.createElement('div');
+
+// add class
+newDiv.className = 'test-style';
+
+// add id
+newDiv.id = 'hello-id';
+
+// add attribute
+newDiv.setAttribute('title', 'Hello div');
+// newDiv.setAttribute('none', '');
+
+// Create text node
+const newDivText = document.createTextNode('Hello World');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+// Insert the div into our header
+
+const container = document.querySelector('header .container');
+const h1 = document.querySelector('header h1');
+
+// insertarlo antes de la etiqueta h1
+container.insertBefore(newDiv, h1);
+
+
+console.log(newDiv);
