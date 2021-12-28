a0b7687d71957750e9a88dab83bc6fbb557d1fcb
Roger Perez <rogelio_perez_98@hotmail.com>
2021-12-28 13:36:26 -0600

    feat: structure for TodoApp

--- JavaScript/JavaScript DOM Crash Course/js/apuntes.js
+++ JavaScript/JavaScript DOM Crash Course/js/apuntes.js
@@ -168,3 +168,26 @@ const handleClick = ( e ) =>{
 }
 
 button.addEventListener('click', handleClick);
+
+
+
+const itemInput = document.querySelector('input[type="text"]');
+
+const form = document.querySelector('form');
+
+let eventName;
+eventName = 'keydown';
+// eventName = 'keyup';
+// eventName = 'keypress';
+
+// eventName = focus;
+// eventName = blur;
+
+
+const runEvent = (e) => {
+    console.log('Event Type: ', e.type);
+    console.log(e.target.value);
+}
+
+itemInput.addEventListener(eventName, runEvent);
+
