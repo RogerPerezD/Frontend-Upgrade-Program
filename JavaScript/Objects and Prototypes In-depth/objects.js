da8c053d04fbef87790aca11754b531afebd62aa
Roger Perez <rogelio_perez_98@hotmail.com>
2021-12-29 10:58:13 -0600

    Using the call function

--- JavaScript/Objects and Prototypes In-depth/objects.js
+++ JavaScript/Objects and Prototypes In-depth/objects.js
@@ -15,3 +15,19 @@ console.log(bicycle1);
 bicycle1.inflateTires();
 console.log(bicycle1);
 
+function Mechanic(name) {
+    this.name = name;
+}
+
+const mike = new Mechanic('Mike');
+
+mike.inflateTires = bicycle1.inflateTires;
+
+// Sirve para pasarle el contexto con el que se debe usar 'this'
+// Al momento de llamar a la funcion, recibe como parametro
+// un objeto, que en este caso es el contexto
+mike.inflateTires.call(bicycle1);
+
+console.log(bicycle1);
+
+
