679f61bd7303f5c75b7d47130b07d13a8b1d752e
Roger Perez <rogelio_perez_98@hotmail.com>
2022-01-03 16:18:59 -0600

    Types exercise

--- TypeScript/Course for Beginners 2021/app.ts
+++ TypeScript/Course for Beginners 2021/app.ts
@@ -1,1 +1,16 @@
-console.log('Hola mundo curel')
+let number1: number;
+number1 =5;
+const number2 = 10;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+function add(n1: number, n2: number, showResult: boolean, phare: string){
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phare + result);
+    }else{
+        return result;
+    }
+}
+
+add(number1, number2, printResult, resultPhrase);
