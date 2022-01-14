c1b82c87ff9e579a14b9a6c20a26f21f5f00902c
Roger Perez <rogelio_perez_98@hotmail.com>
2022-01-14 12:09:42 -0600

    Agregacion de nuevas funcionalidades workshop Chart

--- WebComponents/WebComponetChart/src/index.ts
+++ WebComponents/WebComponetChart/src/index.ts
@@ -1,25 +1,41 @@
 
 import { Chart } from './ts/Chart';
+import { fetchApi } from './ts/fetchApi';
+
 import './css/main.css';
 import './scss/main.scss';
 
 const formChart = document.querySelector('#formChart')! as HTMLFormElement;
 const inputEndPoint = document.querySelector('#endpoint')! as HTMLInputElement;
-const inputTypeChart = document.querySelector('#typeChart')! as HTMLInputElement;
-const inputField = document.querySelector('#field')! as HTMLInputElement;
+const inputTypeChart = document.querySelector('#typeChart')! as HTMLSelectElement;
+const inputField = document.querySelector('#field')! as HTMLSelectElement;
 const inputOptions = document.querySelector('#options')! as HTMLInputElement;
+const containerForm = document.querySelector('.form')! as HTMLElement;
+const btnGetFields = document.querySelector('#getFields')! as HTMLElement;
+// const btnForm = document.querySelector('.form__submit') as HTMLElement;
 
 formChart.addEventListener('submit',(e) =>{ 
     e.preventDefault();
     const mychart = document.createElement('my-chart');
-    mychart.setAttribute('type',inputTypeChart.value);
+    // console.log(inputTypeChart.options[inputTypeChart.selectedIndex].value);
     mychart.setAttribute('data-source',inputEndPoint.value);
-    mychart.setAttribute('field',inputField.value);
+    mychart.setAttribute('field',inputField.options[inputField.selectedIndex].value);
+    mychart.setAttribute('type',inputTypeChart.options[inputTypeChart.selectedIndex].value);
     if (inputOptions.value !== '') {
         mychart.setAttribute('options',inputOptions.value);
     }
-    document.querySelector('body')?.appendChild(mychart);
+    containerForm.removeChild(formChart);
+    containerForm.appendChild(mychart);
     window.customElements.define('my-chart', Chart);
 });
 
-console.log('fue desde type')
+btnGetFields.addEventListener('click', async() =>{
+    const data = await fetchApi(inputEndPoint.value);
+    const keys: string[] = Object.keys(data[0]);
+    keys.forEach(element => {
+        const option = document.createElement('option');
+        option.textContent = element;
+        option.value = element;
+        inputField.appendChild(option);
+    });
+});
