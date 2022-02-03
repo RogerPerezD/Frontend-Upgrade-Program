0e46221a88cc39c5d4868fa9c7b45dfeff711e63
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-03 15:09:46 -0600

    test: mock fuction useNavigate

--- React/heroes-app/src/test/search/SearchScreen.test.tsx
+++ React/heroes-app/src/test/search/SearchScreen.test.tsx
@@ -1,7 +1,15 @@
 import { mount } from "enzyme";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, useNavigate } from 'react-router-dom';
 import { AuthContext } from "../../auth/authContext";
 import { SearchScreen } from "../../components/search/SearchScreen";
+import { FormEvent } from 'react';
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () =>({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate
+}));
 
 describe('Test in <SearchScreen/>', () => {
 
@@ -18,6 +26,52 @@ describe('Test in <SearchScreen/>', () => {
         expect( wrapper.find('.alert').text().trim()).toBe('Search a hero')
     });
 
+    test('should show Batman and the input whit the queyString value', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
 
+        expect( wrapper.find('input').prop('value')).toBe('batman');
+    });
+
+
+    test('should show a message error if doesnt exist the heroe', () => {
+        const query = 'asddfdsf';
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[`/search?q=${query}`]}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('.alert-danger').text().trim()).toBe(`There aren't results for: ${query}`);
+    });
+    
+    test('should show the correct route', () => {
+        const query = 'spider';
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[`/search`]}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        const input = wrapper.find('input').prop('onChange');
+        if(input) {
+            input({
+            currentTarget: {
+                name: 'searchText',
+                value: query
+            } 
+        } as FormEvent<HTMLInputElement>);
+        }
+        
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+        expect(mockNavigate).toBeCalled();
+        expect(mockNavigate).toBeCalledWith(`?q=${query}`);
+
+    });
     
 });
