479373770f41916377a92cb2d3d920459f4478e1
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-10 15:10:03 -0600

    fix: Performance tip, change foreach to for

--- JavaScript/Ajax/js/githubUsers.js
+++ JavaScript/Ajax/js/githubUsers.js
@@ -11,21 +11,22 @@ const fetchUsers = ()=>{
         if (this.readyState === 4 && this.status === 200) {
             const users = JSON.parse(this.responseText);
 
-            users.forEach(({id, login, avatar_url:image}) => {
+            for (let index = 0; index < users.length; index++) {
                 const tr = document.createElement('tr');
                 const tdId = document.createElement('td');
                 const tdLogin = document.createElement('td');
                 const tdImage = document.createElement('td');
                 const imageTag = document.createElement('img');
-                imageTag.src = image;
-                tdId.textContent = id;
-                tdLogin.textContent = login;
+                imageTag.src = users[index].avatar_url;
+                tdId.textContent = users[index].id;
+                tdLogin.textContent = users[index].login;
                 tdImage.append(imageTag);
                 tr.appendChild(tdId);
                 tr.appendChild(tdLogin);
                 tr.appendChild(tdImage);
                 tbody.appendChild(tr);
-            });
+            }
+            
         }
     }
     console.log('object')
