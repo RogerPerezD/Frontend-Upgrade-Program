73bf8ae88546d8e8f201046996544e15a421a527
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-15 16:22:35 -0600

    feat: add validation to the event form

--- React/calendar-app/src/components/calendar/CalendarModal.tsx
+++ React/calendar-app/src/components/calendar/CalendarModal.tsx
@@ -2,7 +2,8 @@ import '../../css/modal.css';
 import Modal from 'react-modal';
 import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
-import { useState } from 'react';
+import Swal from 'sweetalert2';
+import { FormEvent, useState } from 'react';
 
 const customStyles = {
     content: {
@@ -19,10 +20,34 @@ Modal.setAppElement('#root');
 
 const now = moment().minutes(0).seconds(0).add(1, 'hours');
 const future = now.clone().add(1,'hours');
+
+interface Event {
+    title: string;
+    notes: string;
+    start: Date;
+    end: Date;
+};
 export const CalendarModal = () => {
 
     const [dateStart, setDateStart] = useState<Date>(now.toDate());
     const [dateEnd, setDateEnd] = useState<Date>(future.toDate());
+    const [formValues, setFormValues] = useState<Event>({
+        title: 'Evento',
+        notes: '',
+        start: now.toDate(),
+        end: future.toDate()
+    });
+    const [titleValid, setTitleValid] = useState(true);
+
+    const { notes, title, start, end } = formValues;
+
+    const handleInputChange = ({ currentTarget }: FormEvent<(HTMLInputElement|HTMLTextAreaElement)>)=>{
+        // console.log(currentTarget.value);
+        setFormValues({
+            ...formValues,
+            [currentTarget.name]: currentTarget.value
+        })
+    }
 
     const closeModal = ()=>{
         console.log('clossing...');
@@ -31,12 +56,46 @@ export const CalendarModal = () => {
     const handleStartDateChange = ( start: Date )=>{
         console.log(start);
         setDateStart(start);
+        setFormValues({
+            ...formValues,
+            start
+        });
     }
 
     const handleEndDateChange = ( end: Date )=>{
         console.log(end);
         setDateEnd(end);
+        setFormValues({
+            ...formValues,
+            end
+        });
     }
+
+    const handleSubmitForm =(e: FormEvent) =>{
+        e.preventDefault();
+        // console.log(formValues);
+        const momentStart = moment( start );
+        const momentEnd = moment( end );
+        // console.log(momentStart, momentEnd);
+
+        if (momentStart.isSameOrAfter( momentEnd, 'hour' )) {
+            return Swal.fire(
+                'Error',
+                'La fecha fin debe de ser mayor a la fecha de inicio',
+                'error'
+            );
+        }
+
+        if (title.trim().length < 2) {
+            return setTitleValid(false);
+        }
+
+        setTitleValid(true);
+        closeModal();
+    }
+
+
+
 
     return (
         <Modal
@@ -50,7 +109,10 @@ export const CalendarModal = () => {
         >
             <h1> Nuevo evento </h1>
             <hr />
-            <form className="container">
+            <form 
+                className="container"
+                onSubmit={ handleSubmitForm }
+            >
 
                 <div className="form-group">
                     <label>Fecha y hora inicio</label>
@@ -76,10 +138,12 @@ export const CalendarModal = () => {
                     <label>Titulo y notas</label>
                     <input 
                         type="text" 
-                        className="form-control"
+                        className={`form-control ${ !titleValid && 'is-invalid'}`}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
+                        value={ title }
+                        onChange={ handleInputChange }
                     />
                     <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
                 </div>
@@ -90,6 +154,8 @@ export const CalendarModal = () => {
                         placeholder="Notas"
                         rows={5}
                         name="notes"
+                        value={notes}
+                        onChange={ handleInputChange }
                     ></textarea>
                     <small id="emailHelp" className="form-text text-muted">Información adicional</small>
                 </div>
