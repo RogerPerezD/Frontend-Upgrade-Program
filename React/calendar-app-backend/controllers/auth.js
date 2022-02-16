ea20d571e2f1fdf627392012b8909db57db1cc02
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-16 15:11:42 -0600

    feat: add validaiton to the endpoints

--- React/calendar-app-backend/controllers/auth.js
+++ React/calendar-app-backend/controllers/auth.js
@@ -1,8 +1,18 @@
 const { response } = require('express');
+const { validationResult} = require('express-validator');
 
 const createUser = (req , resp = response)=>{
-
     const  {name, email, password}= req.body;
+
+    //handle errrors
+    const errors = validationResult( req );
+    if (!errors.isEmpty()) {
+        return resp.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
     resp.status(201).json({
         ok: true,
         msg: 'register',
@@ -13,11 +23,19 @@ const createUser = (req , resp = response)=>{
 }
 
 const loginUser = (req, resp = response)=>{
-    const  {name, email, password}= req.body;
+    const  { email, password }= req.body;
+    //handle errrors
+    const errors = validationResult( req );
+    if (!errors.isEmpty()) {
+        return resp.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
     resp.json({
         ok: true,
         msg: 'login',
-        name,
         email,
         password
     });
--- React/calendar-app-backend/routes/auth.js
+++ React/calendar-app-backend/routes/auth.js
@@ -5,11 +5,25 @@
 
 const { Router } = require('express');
 const router = Router();
+const { check } = require('express-validator')
 const { createUser, loginUser, revalidateToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
+router.post(
+    '/new', 
+    [//middlewares
+        check('name', 'name is required').not().isEmpty(),
+        check('email', 'email is required').isEmail(),
+        check('password', 'password must have at least 6 characters').isLength({min: 6})
+    ],
+    createUser);
 
-router.post('/', loginUser);
+router.post(
+    '/', 
+    [//middlewares
+        check('email', 'email is required').isEmail(),
+        check('password', 'password must have at least 6 characters').isLength({min: 6})
+    ],
+    loginUser);
 
 router.get('/renew', revalidateToken);
 
