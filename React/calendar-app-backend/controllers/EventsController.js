5e50be4863579f5de4ff585e3067c200707aa2b3
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-17 14:35:48 -0600

    feat: delete events

--- React/calendar-app-backend/controllers/EventsController.js
+++ React/calendar-app-backend/controllers/EventsController.js
@@ -26,23 +26,89 @@ const store = async ( req, resp = response ) =>{
         return resp.status(500).json({
             ok: false,
             msg: 'Server side error'
-        })
+        });
     }
     
 }
 
-const update = ( req, resp = response ) =>{
-    return resp.status(201).json({
-        ok: true,
-        msg: 'update'
-    })
+const update = async ( req, resp = response ) =>{
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+    try {
+        const event = await  Event.findById(eventId);
+        // Validate if the event exist
+        if (!event) {
+            return resp.status(404).json({
+                ok: false,
+                msg: 'Event doesnt exists'
+            });
+        }
+
+        // Validte if the user is enable, to modified the event
+        if (event.user.toString() !== uid) {
+            return resp.status(401).json({
+                ok: false,
+                msg: 'Privileges insufficient'
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent,{ new: true});
+
+        return resp.status(201).json({
+            ok: true,
+            event: eventUpdated
+        });
+    } catch (error) {
+        console.log(error);
+        return resp.status(500).json({
+            ok: false,
+            msg: 'Server side error'
+        });
+    }
+    
 }
 
-const drop = ( req, resp = response ) =>{
-    return resp.status(201).json({
-        ok: true,
-        msg: 'delete'
-    })
+const drop = async ( req, resp = response ) =>{
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await  Event.findById(eventId);
+        // Validate if the event exist
+        if (!event) {
+            return resp.status(404).json({
+                ok: false,
+                msg: 'Event doesnt exists'
+            });
+        }
+
+        // Validte if the user is enable, to modified the event
+        if (event.user.toString() !== uid) {
+            return resp.status(401).json({
+                ok: false,
+                msg: 'Privileges insufficient'
+            });
+        }
+
+        await Event.findByIdAndDelete(eventId);
+
+        return resp.status(203).json({
+            ok: true,
+            status: 'Deleted successfully'
+        });
+    } catch (error) {
+        console.log(error);
+        return resp.status(500).json({
+            ok: false,
+            msg: 'Server side error'
+        });
+    }
 }
 
 
