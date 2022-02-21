067c6f30daa9a4f4c0e14c8f38116ff0c4abe45b
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-21 12:06:10 -0600

    test: auth actions

--- React/calendar-app/src/tests/actions/auth.test.ts
+++ React/calendar-app/src/tests/actions/auth.test.ts
@@ -2,9 +2,15 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { RootState } from '../../store/store';
-import { startLogin } from '../../actions/auth';
-import { AuthAction } from '../../reducers/authReducer';
+import { startLogin, startRegister, startChecking } from '../../actions/auth';
 import { types } from '../../types/types';
+import Swal from 'sweetalert2';
+// import { fetchWithoutToken, fetchWithToken } from '../../helpers/fetch';
+import * as fetchModule from '../../helpers/fetch';
+
+jest.mock('sweetalert2', ()=>({
+    fire: jest.fn()
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore<RootState>(middlewares);
@@ -35,6 +41,78 @@ describe('test in auth action', () => {
         expect( localStorage.setItem ).toHaveBeenCalledWith('token', expect.any(String));
         expect( localStorage.setItem ).toHaveBeenCalledWith('token-init-date', expect.any(String));
 
-        console.log(localStorage.setItem .mock.calls)
+        // console.log(localStorage.setItem.mock.calls)
+        // token =( localStorage.setItem as any).mock.calls
     });
+
+    test('startLogin should fail if the data is wrong', async () => { 
+        await store.dispatch<any>(startLogin('test@mail.com','passWrong'));
+        const actions = store.getActions();
+
+        expect( actions ).toEqual([]);
+        expect( Swal.fire ).toHaveBeenCalledWith("Error", "The password is incorrect", "error");
+    });
+
+    test('startRegister should work', async () => { 
+        const name = 'Roger Test';
+        const uid = '123';
+        const token = 'dgert4e23da';
+
+        (fetchModule as any).fetchWithoutToken = jest.fn(() => ({
+            json(){
+                return {
+                    ok: true,
+                    uid,
+                    name,
+                    token
+                }
+            }
+        }));
+
+        await store.dispatch<any>(startRegister('test2@mail.com','test123','Test2'));
+        const actions = store.getActions();
+        // console.log(actions,'actions');
+
+        expect( actions[0] ).toEqual({
+            type: types.authStartRegister,
+            payload: {
+                _id: uid,
+                name
+            }
+        });
+
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token', token);
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token-init-date', expect.any(String));
+
+    });
+
+    test('startChecking should worl', async () => { 
+        const name = 'Roger Test';
+        const uid = '123';
+        const token = 'dgert4e23da';
+        (fetchModule as any).fetchWithToken = jest.fn(() => ({
+            json(){
+                return {
+                    ok: true,
+                    uid,
+                    name,
+                    token
+                }
+            }
+        }));
+
+        await store.dispatch<any>( startChecking() );
+        const actions = store.getActions();
+        // console.log(actions)
+        expect( actions[0] ).toEqual({
+            type: types.authLogin,
+            payload: {
+                _id: uid,
+                name
+            }
+        });
+        
+        expect( localStorage.setItem ).toHaveBeenCalledWith('token', token);
+    });
+
 })
