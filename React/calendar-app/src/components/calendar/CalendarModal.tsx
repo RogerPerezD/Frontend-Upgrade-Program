52ab1fe77a4e91a2d166da646a99c131ba8180ed
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-21 14:47:16 -0600

    test: in component AppRouter

--- React/calendar-app/src/components/calendar/CalendarModal.tsx
+++ React/calendar-app/src/components/calendar/CalendarModal.tsx
@@ -19,8 +19,9 @@ const customStyles = {
         transform: 'translate(-50%, -50%)',
     }
 };
-
-Modal.setAppElement('#root');
+if (process.env.NODE_ENV !== 'test') {
+    Modal.setAppElement('#root');
+}
 
 const now = moment().minutes(0).seconds(0).add(1, 'hours');
 const future = now.clone().add(1,'hours');
---
+++ React/calendar-app/src/tests/components/routers/AppRouter.test.tsx
@@ -0,0 +1,100 @@
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import { RootState } from '../../../store/store';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { AppRouter } from '../../../components/routers/AppRouter';
+import { EventState } from '../../../reducers/calendarReducer';
+
+const middlewares = [ thunk ];
+const mockStore = configureStore<RootState>(middlewares);
+// const initialStateUser = {
+//     checking: false,
+//     user: {
+//         name: 'Roger',
+//         _id: '123'
+//     }
+// }
+// const initialStateEvents: EventState = {
+//     events: [],
+//     activeEvent: null
+// }
+// const initState = {
+//     auth: initialStateUser,
+//     calendar: initialStateEvents,
+//     ui: {
+//         modalOpen: false
+//     }
+// } as RootState;
+// let store = mockStore(initState);
+// store.dispatch = jest.fn();
+
+
+
+describe('test in AppRouter', () => { 
+
+    test('should display wait...', () => { 
+        const initState = {
+            auth: {
+                checking: true
+            }
+        } as RootState;
+        const store = mockStore(initState);
+        const wrapper = mount(
+                <Provider store = { store }>
+                    <AppRouter/>
+                </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect( wrapper.find('h5').text().trim()).toBe('Wait....');
+    });
+
+    test('should display public route (LoginScreen)', () => { 
+        const initState = {
+            auth: {
+                checking: false
+            }
+        } as RootState;
+        const store = mockStore(initState);
+        const wrapper = mount(
+                <Provider store = { store }>
+                    <AppRouter/>
+                </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect( wrapper.find('.login-container').exists() ).toBe(true);
+    });
+
+    test('should display private route (CalendarScreen)', () => { 
+        const initialStateUser = {
+            checking: false,
+            user: {
+                name: 'Roger',
+                _id: '123'
+            }
+        }
+
+        const initialStateEvents: EventState = {
+                events: [],
+                activeEvent: null
+        }
+
+        const initState = {
+            auth: initialStateUser,
+            calendar: initialStateEvents,
+            ui: {
+                modalOpen: false
+            }
+        } as RootState;
+
+        const store = mockStore(initState);
+        const wrapper = mount(
+                <Provider store = { store }>
+                    <AppRouter/>
+                </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect( wrapper.find('.calendar-screen').exists() ).toBe(true);
+    });
+})
