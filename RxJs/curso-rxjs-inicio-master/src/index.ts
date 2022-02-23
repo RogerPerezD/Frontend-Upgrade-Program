2d103fbf3836ddcfa14fef8f3447b14333c56576
Rogelio Perez <rogelio-antonio.perez@atos.net>
2022-02-23 13:37:32 -0600

    Observer and subscriber

--- RxJs/curso-rxjs-inicio-master/src/index.ts
+++ RxJs/curso-rxjs-inicio-master/src/index.ts
@@ -1,6 +1,25 @@
+import { Observable, Observer } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+const observable$ = new Observable<string>(( subscriber ) =>{
+    subscriber.next('Hola');
+    subscriber.complete();
+    subscriber.next('Mundo');
+});
+
+const observer: Observer<string> = {
+    next: ( value ) =>{ 
+        console.log('Observer got a value: ' + value);
+    },
+    error: ( error )=>{
+        console.error('Observer got a error: ' + error);
+    },
+    complete: ()=>{
+        console.log('Observer got a complete notification');
+    }
+}
+
+observable$.subscribe(observer);
 
 
 
